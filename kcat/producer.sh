32e8918714c8ecb768e4a05863809dd8294682b3
romain amichaud <romain.amichaud@exfo.com>
2022-11-04 10:03:13 +0000

    rework producer kcat

--- kcat/producer.sh
+++ kcat/producer.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-count=3
-
-while ((count > 0))
+while true
 do
-    printf -v date '%(%H:%M:%S)T' -1 
-    printf -v message "message n°%s - %s" "$count" "$date"
-    printf "topic '%s'\n'%s'\n" "$topic" "$message"
+    read -p "> " message
     printf "%s" "$message" | kcat -F "$config" -P -p key1 -t "$topic" -b "$broker"
-    ((count--))
-    sleep 5
 done
 
